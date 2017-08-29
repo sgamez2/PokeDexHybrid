1f5911af1675ee62f816eae37da089618277d565
sgamez2 <gamezsergio.97@gmail.com>
2017-08-29 10:38:53 -0600

    First commit, MVC files

---
+++ PokedexHybridiOS14/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokedexHybridiOS14
+//
+//  Created by Nick Reichard on 8/28/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
